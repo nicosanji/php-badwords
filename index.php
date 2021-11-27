22450347acacc512e4920a27c3afa607957c8ed8
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-11-27 12:30:55 +0100

    DEF. 1.1: aggiunto bootstrap

--- index.php
+++ index.php
@@ -16,21 +16,26 @@ $paragrafoModificato = str_replace($badword, "***", $paragrafo);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP BadWords</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
-  <!-- Paragrafo originale -->
-  <p><strong>Paragrafo originale: </strong><?php echo $paragrafo ?></p>
-  <!-- Lunghezza del paragrafo originale -->
-  <p>Il paragrafo contiene <strong><?php echo strlen($paragrafo) ?></strong> caratteri.</p>
-  <!-- Istruzioni per modificare -->
-  <p>Per censurare una lettera/parola, copia all'interno dell'URL: <strong><u>localhost:8888/php-badwords/index.php?badword=</u></strong><em>lettera/parola che vuoi censurare</em></p>
-  <!-- Mostra lettera/parola censurata -->
-  <p>Hai censurato la lettera/parola: <strong>"<?php echo $badword ?>"</strong>.</p>
-  <!-- Paragrafo censurato -->
-  <p><strong>Paragrafo censurato: </strong><?php echo $paragrafoModificato ?></p>
-  <!-- Lunghezza del paragrafo censurato -->
-  <p>Il paragrafo censurato contiene <strong><?php echo strlen($paragrafoModificato) ?></strong> caratteri.</p>
+  <div class="container py-5">
+    <!-- Paragrafo originale -->
+    <p><strong>Paragrafo originale: </strong><?php echo $paragrafo ?></p>
+    <!-- Lunghezza del paragrafo originale -->
+    <p>Il paragrafo contiene <strong><?php echo strlen($paragrafo) ?></strong> caratteri.</p>
+    <br>
+    <!-- Istruzioni per modificare -->
+    <p>Per censurare una lettera/parola, copia all'interno dell'URL: <strong><u>localhost:8888/php-badwords/index.php?badword=</u></strong><em>lettera/parola che vuoi censurare</em></p>
+    <!-- Mostra lettera/parola censurata -->
+    <p>Hai censurato la lettera/parola: <strong>"<?php echo $badword ?>"</strong>.</p>
+    <br>
+    <!-- Paragrafo censurato -->
+    <p><strong>Paragrafo censurato: </strong><?php echo $paragrafoModificato ?></p>
+    <!-- Lunghezza del paragrafo censurato -->
+    <p>Il paragrafo censurato contiene <strong><?php echo strlen($paragrafoModificato) ?></strong> caratteri.</p>
+  </div>
 </body>
 
 </html>
